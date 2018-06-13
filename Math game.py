8b78249f9f557cfc81eecf49257b525ef7f55050
Cen3mode <40240844+Cen3mode@users.noreply.github.com>
2018-06-13 19:40:44 +0200

    This is my Math game for children v1.0

---
+++ Math game.py
@@ -0,0 +1,57 @@
+import easygui
+
+points = 0
+points = str(points)
+urespons = easygui.buttonbox("How much is 10 * 9?",
+                             title = "Math game " + points + " points",
+                             choices = ("900","90","9"))
+urespons = int(urespons)
+points = int(points)
+
+if urespons == 90:
+    points = points + 1
+    points = str(points)
+    easygui.msgbox("correct")
+    urespons = easygui.buttonbox("How much is 28 : 4?",
+                                 title = "Math game " + points + " points",
+                                 choices = ("7","3","14"))
+else:
+    easygui.msgbox("incorrect")
+    points = str(points)
+    urespons = easygui.buttonbox("How much is 28 : 4?",
+                                 title = "Math game " + points + " points",
+                                 choices = ("7","3","14"))
+points = int(points)
+urespons = int(urespons)
+
+if urespons == 7:
+    points = points + 1
+    points = str(points)
+    easygui.msgbox("correct")
+    urespons = easygui.buttonbox("How much is 100 : 100 *100?",
+                                 title = "Math game " + points + " points",
+                                 choices = ("0","10000","100"))
+else:
+    easygui.msgbox("incorrect")
+    points = str(points)
+    urespons = easygui.buttonbox("How much is 100 : 100 *100?",
+                                 title = "Math game " + points + " points",
+                                 choices = ("0","10000","100"))
+points = int(points)
+urespons = int(urespons)
+
+if urespons == 100:
+    points = points + 1
+    points = str(points)
+    easygui.msgbox("correct")
+else:
+    easygui.msgbox("incorrect")
+
+points = int(points)
+
+if points > 1:
+    points = str(points)
+    easygui.msgbox("You are good your points are " + points)
+elif points <= 1:
+    points = int(points)
+    easygui.msgbox("Try again")
