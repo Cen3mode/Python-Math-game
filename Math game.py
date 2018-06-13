c7902d0055d03f8b29beeefa767d10707ce98443
Cen3mode <40240844+Cen3mode@users.noreply.github.com>
2018-06-13 22:00:25 +0200

    A little update on the game :)

--- Math game.py
+++ Math game.py
@@ -1,57 +1,65 @@
 import easygui
 
-points = 0
-points = str(points)
-urespons = easygui.buttonbox("How much is 10 * 9?",
-                             title = "Math game " + points + " points",
-                             choices = ("900","90","9"))
-urespons = int(urespons)
-points = int(points)
+def mathquiz():
+    points = 0
 
-if urespons == 90:
-    points = points + 1
     points = str(points)
-    easygui.msgbox("correct")
-    urespons = easygui.buttonbox("How much is 28 : 4?",
+    urespons = easygui.buttonbox("How much is 10 * 9?",
                                  title = "Math game " + points + " points",
-                                 choices = ("7","3","14"))
-else:
-    easygui.msgbox("incorrect")
-    points = str(points)
-    urespons = easygui.buttonbox("How much is 28 : 4?",
-                                 title = "Math game " + points + " points",
-                                 choices = ("7","3","14"))
-points = int(points)
-urespons = int(urespons)
+                                 choices = ("900","90","9"))
+    urespons = int(urespons)
+    points = int(points)
 
-if urespons == 7:
-    points = points + 1
-    points = str(points)
-    easygui.msgbox("correct")
-    urespons = easygui.buttonbox("How much is 100 : 100 *100?",
-                                 title = "Math game " + points + " points",
-                                 choices = ("0","10000","100"))
-else:
-    easygui.msgbox("incorrect")
-    points = str(points)
-    urespons = easygui.buttonbox("How much is 100 : 100 *100?",
-                                 title = "Math game " + points + " points",
-                                 choices = ("0","10000","100"))
-points = int(points)
-urespons = int(urespons)
+    if urespons == 90:
+        points = points + 1
+        points = str(points)
+        easygui.msgbox("correct")
+        urespons = easygui.buttonbox("How much is 28 : 4?",
+                                     title = "Math game " + points + " points",
+                                     choices = ("7","3","14"))
+    else:
+        easygui.msgbox("incorrect")
+        points = str(points)
+        urespons = easygui.buttonbox("How much is 28 : 4?",
+                                     title = "Math game " + points + " points",
+                                     choices = ("7","3","14"))
+    points = int(points)
+    urespons = int(urespons)
 
-if urespons == 100:
-    points = points + 1
-    points = str(points)
-    easygui.msgbox("correct")
-else:
-    easygui.msgbox("incorrect")
+    if urespons == 7:
+        points = points + 1
+        points = str(points)
+        easygui.msgbox("correct")
+        urespons = easygui.buttonbox("How much is 100 : 100 *100?",
+                                     title = "Math game " + points + " points",
+                                     choices = ("0","10000","100"))
+    else:
+        easygui.msgbox("incorrect")
+        points = str(points)
+        urespons = easygui.buttonbox("How much is 100 : 100 *100?",
+                                     title = "Math game " + points + " points",
+                                     choices = ("0","10000","100"))
+    points = int(points)
+    urespons = int(urespons)
 
-points = int(points)
+    if urespons == 100:
+        points = points + 1
+        points = str(points)
+        easygui.msgbox("correct")
+    else:
+        easygui.msgbox("incorrect")
+        
+    urespons = str(urespons)
+    points = int(points)
+    return points
+
+points = mathquiz()
 
 if points > 1:
     points = str(points)
     easygui.msgbox("You are good your points are " + points)
 elif points <= 1:
     points = int(points)
-    easygui.msgbox("Try again")
+    urespons = easygui.buttonbox(choices = ("ok","try again"))
+    if urespons == "try again":
+        mathquiz()
